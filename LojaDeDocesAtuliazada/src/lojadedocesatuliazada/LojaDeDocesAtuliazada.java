ea023e2100b02cc8b34f5d0d910104b7612769c3
Abdiel352 <120688631+Abdiel352@users.noreply.github.com>
2024-08-07 15:23:05 -0400

    Add files via upload

---
+++ LojaDeDocesAtuliazada/src/lojadedocesatuliazada/LojaDeDocesAtuliazada.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lojadedocesatuliazada;
+
+/**
+ *
+ * @author Aluno
+ */
+import javax.swing.JFrame;
+public class LojaDeDocesAtuliazada {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Doces frame = new Doces();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,200);
+        frame.setVisible(true);
+    }
+    
+}
